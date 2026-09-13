e48c2edd7107e23e8683ca65e8f1f0d3bdd218f7
gabriel <gabrieldeolival2@gmail.com>
2026-09-13 17:52:53 -0400

    build(eslint): add eslint configuration for astro and typescript

---
+++ eslint.config.ts
@@ -0,0 +1,22 @@
+import eslintAstro from "eslint-plugin-astro";
+import tseslint from 'typescript-eslint';
+
+export default [
+	{
+    ignores: ['.astro/**', 'dist/**', 'node_modules/**']
+  },
+	// Recommended Rules for TypeScript
+	...tseslint.configs.recommended,
+
+	// Recommended Rules for JavaScript
+	...eslintAstro.configs['flat/recommended'],
+	{
+    files: ['**/*.astro'],
+    languageOptions: {
+      parserOptions: {
+        parser: tseslint.parser,
+        extraFileExtensions: ['.astro']
+      }
+    }
+  }
+];
