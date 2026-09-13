d832af937fbefed9a1c2a82ef92e85cbd8d31864
gabriel <gabrieldeolival2@gmail.com>
2026-09-12 23:14:06 -0400

    fix(cls): add fonts to astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,5 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig, fontProviders } from 'astro/config';
 import tailwindcss from "@tailwindcss/vite";
 import icon from "astro-icon";
 
@@ -8,5 +8,17 @@ export default defineConfig({
 	integrations: [icon()],
 	vite: {
 		plugins: [tailwindcss()]
-	}
+	},
+	fonts: [
+		{
+			provider: fontProviders.fontsource(),
+			name: 'Poppins',
+			cssVariable: "--font-poppins"
+		},
+		{
+			provider: fontProviders.fontsource(),
+			name: 'DM Sans',
+			cssVariable: "--font-dm-sans"
+		}
+	]
 });
