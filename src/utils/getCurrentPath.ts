071b4d2e60e02fbbb403c82fd52e9e25e36c3c9b
gabriel <gabrieldeolival2@gmail.com>
2026-09-12 23:15:25 -0400

    refactor: extract the validation for the path to a new utility

---
+++ src/utils/getCurrentPath.ts
@@ -0,0 +1,11 @@
+
+/**
+ * Verify if the path is active.
+ * 
+ * @param pathname actual path
+ * @param href path to validate
+ * @returns boolean
+ */
+export function pathIsActive(pathname: string, href: string): boolean {
+	return pathname === href || (href !== '/' && pathname.startsWith(href));
+}
